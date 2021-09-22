92b200437f8bd0ee46d28aa19832dc21c160bcfb
mxs <mxs@gduf.com>
2021-09-22 18:17:09 +0800

    third commit

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
